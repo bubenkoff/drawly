4282c321aceee663d7682c4e83c1585c7a7d89a7
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-10-26 21:25:45 +0000

    build assets

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 """Management entry point script."""
 from flask.ext.script import Manager, Server
 
-from drawly.app import socketio, app
+from drawly.app import app, assets
 
 manager = Manager(app)
 
@@ -9,5 +9,10 @@ app.config['DEBUG'] = True
 
 manager.add_command('runserver', Server(host="0.0.0.0", port=8000))
 
+from flask.ext.assets import ManageAssets
+manager = Manager(app)
+
+manager.add_command("assets", ManageAssets(assets))
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000)
+    manager.run()
